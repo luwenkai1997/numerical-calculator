71a485d6c85252d7fe3d0e7637a6afe331a9d450
Wenkai Lu <wenkailu@wenkaidemacbook-pro.local>
2019-03-20 17:42:30 -0700

    Initial Commit

---
+++ PIC_10C_Final_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PIC_10C_Final_Project
+//
+//  Created by Wenkai Lu on 3/20/19.
+//  Copyright © 2019 Wenkai Lu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
