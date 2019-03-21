20d9331ae84f5bfabc1fa6ff109b19cc90e12c8c
Wenkai Lu <wenkailu@wenkaidemacbook-pro.local>
2019-03-20 18:59:35 -0700

    Now the code can solve polynomial functions with degree 1 or 2.
    Nest step: degree 3 and 4

--- PIC_10C_Final_Project/main.cpp
+++ PIC_10C_Final_Project/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <vector>
 #include "solve_eq.h"
 #include "calculus.h"
-
 
 
 int main() {
@@ -12,7 +12,7 @@ int main() {
     // Ask the user to choose the function
     std::cout << "Welcome to use Wenkai's calculator! \n";
     std::cout << "Here are the functions of this calculator: \n\n";
-    std::cout << "1. Solving Polynomial Equations \n";
+    std::cout << "1. Solving Polynomial Equations in R \n";
     std::cout << "2. Interpolation of Data \n";
     std::cout << "3. Estimating Derivaticves \n";
     std::cout << "4. Integtation of Data \n\n";
@@ -21,10 +21,46 @@ int main() {
     
     // check whether the input is valid
     while (mode != 1 && mode != 2 && mode && 3 && mode != 4) {
-        std::cout << "Invalid input! Please enter 1, 2, 3, or 4: ";
+        std::cout << "Invalid input! Please enter 1, 2, 3, 4: ";
         std::cin >> mode;
     }
     
+    
+    // Function 1: Solving Polynomial Equations in R
+    if (mode == 1) {
+        int n = 0; // degree of the polynomial
+        std::cout << "Please enter the degree of the polynomial function you want to solve (up to 4): ";
+        std::cin >> n;
+        
+        // check whether the input is valid
+        while (n != 1 && n != 2 && n != 3 && n != 4) {
+            std::cout << "Invalid input! Please enter 1, 2, 3, 4: ";
+            std::cin >> n;
+        }
+        
+        // ask for coefficients of the polynomial
+        std::cout << "The function is in the form: f(x) = a_0 + a_1 * x + a_2 * x^2 + a_3 * x^3 + a_4 * x^4 = 0 \n";
+        std::vector<double> coeff(n+1);
+        for (int i = 0; i < n+1; ++i) {
+            std::cout << "Please enter the value of a_" << i << ": ";
+            std::cin >> coeff[i];
+        }
+        
+        // if this function has degree 1...
+        if (n == 1) {
+            std::cout << "The root of this function is " << -1 * coeff[0] / coeff[1] << "\n\n";
+            return 0;
+        }
+        
+        // if this function has degree 2...
+        if (n == 2) {
+            solve_quadratic_eq(coeff);
+            return 0;
+        }
+        
+        // if this function has degree higher 3 or 4...
+        
+    }
     
     
     
--- PIC_10C_Final_Project/solve_eq.h
+++ PIC_10C_Final_Project/solve_eq.h
@@ -1,6 +1,22 @@
 #ifndef solve_eq_h
 #define solve_eq_h
 
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+// Solve polynomial equations with degree 2
+void solve_quadratic_eq(const std::vector<double>& coeff) {
+    std::cout << std::fixed;
+    std::cout << std::setprecision(3);
+    double delta = pow(coeff[1],2) - 4 * coeff[0] * coeff[2];
+    double x1 = (-1 * coeff[1] + sqrt(delta)) / (2 * coeff[2]);
+    double x2 = (-1 * coeff[1] - sqrt(delta)) / (2 * coeff[2]);
+    std::cout << "The roots of this equation are " << x1 << " and " << x2 << "\n\n";
+}
+
+
 
 
 #endif
