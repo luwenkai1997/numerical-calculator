eee25ed3f072e00f9f850b4b92a896890512d8a1
Wenkai Lu <wenkailu@wenkaidemacbook-pro.local>
2019-03-20 18:05:55 -0700

    Ask the user for input. Then run different functions based on the mode that the user chooses.

--- PIC_10C_Final_Project/main.cpp
+++ PIC_10C_Final_Project/main.cpp
@@ -1,15 +1,37 @@
-//
-//  main.cpp
-//  PIC_10C_Final_Project
-//
-//  Created by Wenkai Lu on 3/20/19.
-//  Copyright © 2019 Wenkai Lu. All rights reserved.
-//
-
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+#include "solve_eq.h"
+#include "calculus.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+
+int main() {
+    int mode = 0;
+    
+    // Ask the user to choose the function
+    std::cout << "Welcome to use Wenkai's calculator! \n";
+    std::cout << "Here are the functions of this calculator: \n\n";
+    std::cout << "1. Solving Polynomial Equations \n";
+    std::cout << "2. Interpolation of Data \n";
+    std::cout << "3. Estimating Derivaticves \n";
+    std::cout << "4. Integtation of Data \n\n";
+    std::cout << "Please enter the function you want to use: ";
+    std::cin >> mode;
+    
+    // check whether the input is valid
+    while (mode != 1 && mode != 2 && mode && 3 && mode != 4) {
+        std::cout << "Invalid input! Please enter 1, 2, 3, or 4: ";
+        std::cin >> mode;
+    }
+    
+    
+    
+    
+    
+    
     return 0;
 }
+
+
+
