cb33e2b261d02f3a884bbce29cedd188d2a7b765
Wenkai Lu <gary.luwenkai@gmail.com>
2019-03-27 01:14:23 -0700

    Write function 4 for this program.

--- PIC_10C_Final_Project/calculus.h
+++ PIC_10C_Final_Project/calculus.h
@@ -5,6 +5,7 @@
 // Estimate derivative at t through Lagrange Polynomials
 double differentiate(const double& t, const std::vector<double>& x, const std::vector<double>& y);
 
+double composite(const std::vector<double>& x, const std::vector<double>& y, int n);
 
 
 
@@ -16,6 +17,30 @@ double differentiate(const double& t, const std::vector<double>& x, const std::v
     return derivative;
 }
 
+
+double composite(const std::vector<double>& x, const std::vector<double>& y, int n) {
+    double a = x[0];
+    double b = x[n-1];
+    double h = (b-a)/n;
+    double integral = 0;
+    
+    for (int i = 0; i < n; ++i) {
+        if (i == 0) {
+            integral = integral + h/3 * y[i];
+        }
+        else if (i == n-1) {
+            integral = integral + h/3 * y[i];
+        }
+        else if (i % 2 == 0) {
+            integral = integral + 2*h/3 * y[i];
+        }
+        else {
+            integral = integral + 4*h/3 * y[i];
+        }
+    }
+    
+    return integral;
+}
 
 
 #endif 
--- PIC_10C_Final_Project/main.cpp
+++ PIC_10C_Final_Project/main.cpp
@@ -219,11 +219,12 @@ int main() {
     if (mode == 4) {
         int n = 0;
         std::cout << "Welcome to integration of data! \n";
-        std::cout << "How many data point do you have? (up to 5) ";
+        std::cout << "How many data point do you have? (up to 10) ";
         std::cin >> n;
         std::vector<double> x(n);
         std::vector<double> y(n);
         std::cout << "Now, please enter the data points following instructions. \n";
+        std::cout << "The x data must seperate the domain into n equal intervals! \n";
         
         for (int i = 0; i < n; ++i) {
             std::cout << "Value of x_" << i << ": ";
@@ -232,7 +233,8 @@ int main() {
             std::cin >> y[i];
         }
         
-        
+        std::cout << "The estimated integral of the function y(x) is: " << composite(x, y, n) << " \n";
+        std::cout << "The estimation may have error depending on the data. \n\n";
         
     }
     
